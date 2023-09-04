38d4abab51732bb0032529798a3125db4e259041
박건우 <white0581@naver.com>
2023-09-05 02:12:01 +0900

    create pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
-  return <h1 className="text-2xl bg-red-400">안녕하세요</h1>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App
