5b6a0f85cd10c4a9034f0bdefd6e005c93b48276
박건우 <white0581@naver.com>
2023-09-05 11:23:55 +0900

    create the header component

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import Offers from './pages/Offers'
 import ForgotPassword from './pages/ForgotPassword'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
