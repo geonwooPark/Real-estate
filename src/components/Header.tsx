8eca71d0dc63ee18822c9696c60c24e12352d230
박건우 <white0581@naver.com>
2023-09-20 21:28:50 +0900

    complete the responsive header components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,78 +23,77 @@ export default function Header() {
   }
 
   return (
-    <div className="bg-white border-b sticky top-0 z-50">
-      <header className="flex flex-col justify-between px-4 sm:max-w-6xl sm:mx-auto sm:flex-row">
-        <div className="w-full border-b flex items-center justify-between z-99 sm:w-auto sm:border-none ">
-          <img
-            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
-            alt="logo"
-            className="h-5 my-3 cursor-pointer"
-            onClick={() => navigate('/')}
-          />
-          <MenuIcon
-            showMenu={showMenu}
-            setShowMenu={setShowMenu}
-            className={'sm:hidden'}
-          />
-        </div>
+    <header className="bg-white border-b sticky top-0 z-50 px-4 sm:max-w-6xl sm:mx-auto sm:flex sm:flex-row sm:justify-between">
+      <div className="w-full border-b flex items-center justify-between sm:w-auto sm:border-none">
+        <img
+          src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+          alt="logo"
+          className="h-5 my-3 cursor-pointer"
+          onClick={() => navigate('/')}
+        />
+        <MenuIcon
+          showMenu={showMenu}
+          setShowMenu={setShowMenu}
+          className={'sm:hidden'}
+        />
+      </div>
 
-        <div>
-          <ul className="bg-white flex flex-col text-center sm:flex-row sm:space-x-10 px-0">
-            {loggedIn ? (
-              <>
-                <li
-                  className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
-                    pathMatchRoute('/contact') &&
-                    'sm:border-b-red-400 !text-black'
-                  }`}
-                  onClick={() => navigate('/contact')}
+      <div
+        className={`absolute sm:relative left-0 w-full sm:w-auto transition-transform duration-200 ease-in-out shadow-md sm:shadow-none ${
+          showMenu ? 'translate-y-0' : '-translate-y-[300px]'
+        } sm:translate-y-0`}
+      >
+        <ul className="bg-white px-0 flex flex-col sm:flex-row sm:space-x-10 text-center">
+          {loggedIn ? (
+            <>
+              <li
+                className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
+                  pathMatchRoute('/contact') && 'sm:border-b-black !text-black'
+                }`}
+                onClick={() => navigate('/contact')}
+              >
+                메시지
+              </li>
+              <li
+                className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
+                  pathMatchRoute('/profile') && 'sm:border-b-black !text-black'
+                }`}
+                onClick={() => navigate('/profile')}
+              >
+                프로필
+              </li>
+              <li
+                className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
+                  pathMatchRoute('/create-listing') &&
+                  'sm:border-b-black !text-black'
+                }`}
+                onClick={() => navigate('/create-listing')}
+              >
+                매물등록
+              </li>
+              <li className="py-3 text-sm border-b-[3px] border-b-transparent">
+                <button
+                  onClick={onLogout}
+                  className="text-red-400 rounded-md transition duration-200 ease-in-out"
                 >
-                  메시지
-                </li>
-                <li
-                  className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
-                    pathMatchRoute('/profile') &&
-                    'sm:border-b-red-400 !text-black'
-                  }`}
-                  onClick={() => navigate('/profile')}
-                >
-                  프로필
-                </li>
-                <li
-                  className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
-                    pathMatchRoute('/create-listing') &&
-                    'sm:border-b-red-400 !text-black'
-                  }`}
-                  onClick={() => navigate('/create-listing')}
-                >
-                  매물등록
-                </li>
-                <li className="py-3 text-sm border-b-[3px] border-b-transparent">
-                  <button
-                    onClick={onLogout}
-                    className="text-red-400 rounded-md transition duration-200 ease-in-out"
-                  >
-                    로그아웃
-                  </button>
-                </li>
-              </>
-            ) : (
-              !checkingStatus && (
-                <li
-                  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                    pathMatchRoute('/sign-in') &&
-                    '!border-b-red-400 !text-black'
-                  }`}
-                  onClick={() => navigate('/sign-in')}
-                >
-                  로그인
-                </li>
-              )
-            )}
-          </ul>
-        </div>
-      </header>
-    </div>
+                  로그아웃
+                </button>
+              </li>
+            </>
+          ) : (
+            !checkingStatus && (
+              <li
+                className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                  pathMatchRoute('/sign-in') && '!border-b-black !text-black'
+                }`}
+                onClick={() => navigate('/sign-in')}
+              >
+                로그인
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    </header>
   )
 }
