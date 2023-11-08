62f8142f45522ca1550b76a29e845442ffe1721d
박건우 <white0581@naver.com>
2023-11-08 13:40:43 +0900

    Header 컴포넌트 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate, useLocation, Link } from 'react-router-dom'
 import useAuthStatus from '../hooks/useAuthStatus'
 import { auth } from '../firebase'
 import MenuIcon from './MenuIcon'
 import { useAppSelector } from '../store/store'
-// import { MapContext } from '../App'
 
 const { kakao } = window as any
 
@@ -13,7 +12,8 @@ export default function Header() {
 
   const location = useLocation()
   const navigate = useNavigate()
-  const { loggedIn, setLoggedIn, checkingStatus } = useAuthStatus()
+
+  const { loggedIn, unread, setLoggedIn, checkingStatus } = useAuthStatus()
   const [showMenu, setShowMenu] = useState(false)
 
   const goHome = () => {
@@ -40,80 +40,108 @@ export default function Header() {
   }
 
   return (
-    <header
-      className={`bg-white border-b sticky w-full h-[48px] top-0 z-50 sm:max-w-6xl sm:mx-auto sm:flex sm:justify-between
-                  ${!showMenu && 'overflow-hidden sm:overflow-auto'}
-                `}
-    >
-      <div className="w-full h-[48px] px-4 border-b flex items-center justify-between sm:w-auto sm:border-none">
-        <div onClick={goHome} className="font-bold text-3xl cursor-pointer">
-          부동산직거래
+    <header className="border-b">
+      <div
+        className={`bg-white sticky w-full h-[48px] px-4 top-0 z-50 sm:max-w-6xl sm:mx-auto sm:flex sm:justify-between`}
+      >
+        <div className="w-full h-full flex items-center justify-between sm:w-auto sm:border-none">
+          <div onClick={goHome} className="font-bold text-2xl cursor-pointer">
+            부동산직거래
+          </div>
+          <MenuIcon
+            showMenu={showMenu}
+            setShowMenu={setShowMenu}
+            className={'sm:hidden'}
+          />
         </div>
-        <MenuIcon
-          showMenu={showMenu}
-          setShowMenu={setShowMenu}
-          className={'sm:hidden'}
-        />
-      </div>
-
-      <div className="overflow-hidden border-b sm:border-none">
-        <div
-          className={`w-full transition-transform duration-200 ease-in-out sm:translate-y-0 bg-white
-            ${showMenu ? 'translate-y-0' : '-translate-y-full'}
-          `}
-        >
-          <ul className="px-4 flex flex-col sm:flex-row sm:gap-10 sm:justify-end text-center">
+        <div className="h-full hidden sm:block">
+          <ul className="h-full text-sm flex gap-4 text-gray-400">
             {loggedIn ? (
               <>
                 <li
-                  className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
+                  className={`border-b-[3px] border-b-transparent ${
                     pathMatchRoute('/create-listing') &&
-                    'sm:border-b-black !text-black'
+                    'sm:border-b-black text-black'
                   }`}
-                  onClick={() => navigate('/create-listing')}
                 >
-                  매물등록
+                  <Link to={'/create-listing'} className="block leading-[48px]">
+                    매물등록
+                  </Link>
                 </li>
                 <li
-                  className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
-                    pathMatchRoute('/chat') && 'sm:border-b-black !text-black'
+                  className={`relative border-b-[3px] border-b-transparent ${
+                    pathMatchRoute('/chat') && 'sm:border-b-black text-black'
                   }`}
-                  onClick={() => navigate('/chat')}
                 >
-                  메시지
+                  <Link to={'/chat'} className="block leading-[48px]">
+                    메시지
+                  </Link>
+                  {unread.length !== 0 && (
+                    <span className="absolute top-2 -right-2 flex h-3 w-3">
+                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75"></span>
+                      <span className="relative inline-flex rounded-full h-3 w-3 bg-blue-500"></span>
+                    </span>
+                  )}
                 </li>
                 <li
-                  className={`cursor-pointer py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${
-                    pathMatchRoute('/profile') &&
-                    'sm:border-b-black !text-black'
+                  className={`border-b-[3px] border-b-transparent ${
+                    pathMatchRoute('/profile') && 'sm:border-b-black text-black'
                   }`}
-                  onClick={() => navigate('/profile')}
                 >
-                  프로필
+                  <Link to={'/profile'} className="block leading-[48px]">
+                    프로필
+                  </Link>
                 </li>
-                <li className="py-3 text-sm border-b-[3px] border-b-transparent">
-                  <button
-                    onClick={onLogout}
-                    className="text-red-400 rounded-md transition duration-200 ease-in-out"
-                  >
+                <li className="text-red-400">
+                  <button onClick={onLogout} className="block h-full">
                     로그아웃
                   </button>
                 </li>
               </>
             ) : (
-              !checkingStatus && (
-                <li
-                  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                    pathMatchRoute('/sign-in') && '!border-b-black !text-black'
-                  }`}
-                  onClick={() => navigate('/sign-in')}
-                >
+              <li
+                className={`text-gray-400 border-b-[3px] border-b-transparent ${
+                  pathMatchRoute('/sign-in') && 'sm:border-b-black !text-black'
+                }`}
+              >
+                <Link to={'/sign-in'} className="block leading-[48px]">
                   로그인
-                </li>
-              )
+                </Link>
+              </li>
             )}
           </ul>
         </div>
+      </div>
+
+      <div
+        className={`absolute z-[40] w-full border-b transition sm:hidden
+        ${showMenu ? 'translate-y-0' : '-translate-y-full'}
+      `}
+      >
+        <ul className="text-center text-sm bg-gray-50 text-gray-400">
+          <li
+            className={`${pathMatchRoute('/create-listing') && 'text-black'}`}
+          >
+            <Link to={'/create-listing'} className="block py-2">
+              매물등록
+            </Link>
+          </li>
+          <li className={`${pathMatchRoute('/chat') && 'text-black'}`}>
+            <Link to={'/chat'} className="block py-2">
+              메시지
+            </Link>
+          </li>
+          <li className={`${pathMatchRoute('/profile') && 'text-black'}`}>
+            <Link to={'/profile'} className="block py-2">
+              프로필
+            </Link>
+          </li>
+          <li className="text-red-400">
+            <button onClick={onLogout} className="block w-full h-full py-2">
+              로그아웃
+            </button>
+          </li>
+        </ul>
       </div>
     </header>
   )
